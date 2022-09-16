50239abfbbb6f25d988da2e0b52be57f0d32389b
mohammadammash <mohammadammash31@gmail.com>
2022-09-16 17:33:47 +0300

    Showing and affecting score, increasing it by 1 if user match and decrase by 2 if user don't, in '/js/app.js'

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 const cards = document.getElementsByClassName("card");
 const logos = document.getElementsByClassName("programming-logo");
+const scoreShow = document.getElementById("score");
 
 let hasFlippedCard = false;
 let firstCard, secondCard, firstCard_imgsrc, secondCard_imgsrc;
-let score = 0;
+let score = parseInt(scoreShow.textContent);
 // to make sure not more than 2 cards are flipped at same time
 let flippedNow = false;
 
 function flipCard() {
-  // add class flipCard to the card clicked(flip-it to show logo)
+  // add class flipCard to the card clicked(flip-it to show logo)(we used firstChildNode to flip the specific images not the whole card)
   if (!flippedNow) {
     this.childNodes[1].classList.add("flip-card");
     if (!hasFlippedCard) {
-      // first flip
+      // first flip (saving card main div and card-back image source)
       hasFlippedCard = true;
       firstCard = this;
       firstCard_imgsrc = this.childNodes[1].childNodes[3].childNodes[1].src;
     } else {
-      //second flip
+      //second flip (saving card main div and card-back image source)
       secondCard = this;
       secondCard_imgsrc = this.childNodes[1].childNodes[3].childNodes[1].src;
 
@@ -25,19 +26,26 @@ function flipCard() {
       if (firstCard_imgsrc === secondCard_imgsrc) {
         flippedNow = true;
         setTimeout(() => {
-        firstCard.childNodes[1].classList.add('display-none');
-        secondCard.childNodes[1].classList.add('display-none')
-        score += 1;
-        flippedNow = false;
-        },1800);
+          firstCard.childNodes[1].classList.add("display-none");
+          secondCard.childNodes[1].classList.add("display-none");
+          score += 1;
+          flippedNow = false;
+          // update score show
+          scoreShow.textContent = score;
+        }, 1800);
       } else {
         //not a match - setTimeout to reflip cards after specific time, so user can view both cards
         flippedNow = true;
         setTimeout(() => {
           firstCard.childNodes[1].classList.remove("flip-card");
           secondCard.childNodes[1].classList.remove("flip-card");
+          // return flipping to be able
           flippedNow = false;
-          score-=2;
+          score -= 2;
+          // eliminate option of having negative score
+          if (score < 0) score = 0;
+          // update score show
+          scoreShow.textContent = score;
         }, 1500);
       }
 
