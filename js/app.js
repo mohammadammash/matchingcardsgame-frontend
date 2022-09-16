d6829d0034e44748d8c5ac30f85f7d999d999291
mohammadammash <mohammadammash31@gmail.com>
2022-09-16 17:02:48 +0300

    Adding 'flippedNow'-boolean to make sure game wouldn't turn cards until all cards return to default state

--- js/app.js
+++ js/app.js
@@ -2,40 +2,43 @@ const cards = document.getElementsByClassName("card");
 const logos = document.getElementsByClassName("programming-logo");
 
 let hasFlippedCard = false;
-let firstCard, secondCard;
+let firstCard, secondCard, firstCard_imgsrc, secondCard_imgsrc;
 let score = 0;
+// to make sure not more than 2 cards are flipped at same time
+let flippedNow = false;
 
 function flipCard() {
   // add class flipCard to the card clicked(flip-it to show logo)
+if(!flippedNow){  
   this.childNodes[1].classList.add("flip-card");
-  let firstCard_imgsrc, secondCard_imgsrc;
-
-  if (!hasFlippedCard) {
-    // first flip
-    hasFlippedCard = true;
-    firstCard = this;
-    firstCard_imgsrc = this.childNodes[1].childNodes[3].childNodes[1].src;
-  } else {
-    //second flip
-    secondCard = this;
-    secondCard_imgsrc = this.childNodes[1].childNodes[3].childNodes[1].src;
-    
-    //after second flip:
-    // if cards matches: (score + 1 and hide cards)
-    if (firstCard_imgsrc === secondCard_imgsrc) {
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-      score+=1;
+    if (!hasFlippedCard) {
+      // first flip
+      hasFlippedCard = true;
+      firstCard = this;
+      firstCard_imgsrc = this.childNodes[1].childNodes[3].childNodes[1].src;
     } else {
-      //not a match - setTimeout to reflip cards after 1.8 seconds, so user can view both cards
-      setTimeout(()=>{
-        firstCard.childNodes[1].classList.remove("flip-card");
-      secondCard.childNodes[1].classList.remove("flip-card");
-      console.log("oops");
-      },1800);
-    }
+      //second flip
+      secondCard = this;
+      secondCard_imgsrc = this.childNodes[1].childNodes[3].childNodes[1].src;
 
-    hasFlippedCard = false; //reset the comparison
+      // if cards matches: (score + 1 and remove event listener so the user cannot cheat)
+      if (firstCard_imgsrc === secondCard_imgsrc) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+        score += 1;
+      } else {
+        //not a match - setTimeout to reflip cards after specific time, so user can view both cards
+        flippedNow = true;
+        setTimeout(() => {
+          firstCard.childNodes[1].classList.remove("flip-card");
+          secondCard.childNodes[1].classList.remove("flip-card");
+          flippedNow = false;
+        }, 1500);
+      }
+
+      console.log(score);
+      hasFlippedCard = false; //reset the comparison
+    }
   }
 }
 
